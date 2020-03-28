ff4881224cbaf1c044673a82302f49bb7feff1fa
moejoe <moejoe@users.noreply.github.com>
2020-03-28 14:40:21 +0100

    apply formgroup styling

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -28,7 +28,8 @@ const routes: Routes = [
   },
   {
     path: 'createHelfer',
-    component: CreateHelferComponent
+    component: CreateHelferComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: "**",
